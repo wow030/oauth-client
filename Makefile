71850ec098bbc678ebdbc1a3fe5af0c36eafc99d
wow030 <zaq05206@gmail.com>
2019-11-17 21:41:54 +0800

    add token url

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 run:
-	DEDIRECT_URL=https://localhost:8080/redirect
-	go run main.go
+	REDIRECT_URL=https://localhost:8080/redirect \
+	TOKEN_URL=https://localhost:8081/token \
+	go run .
 
 test:
 	go test -count=1 -p 1 -v ./service/...
--- service/service.go
+++ service/service.go
@@ -1,11 +1,18 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 type Service struct {
-	Mux *http.ServeMux
+	Mux      *http.ServeMux
+	tokenURL string
+}
+
+type AuthConfig struct {
+	TokenURL string
 }
 
 func CreateService() *Service {
@@ -13,12 +20,17 @@ func CreateService() *Service {
 
 	service.Mux = http.NewServeMux()
 
-	service.Mux.HandleFunc("/guest", GuestHandler)
+	service.Mux.HandleFunc("/guest", service.GuestHandler)
 
+	service.tokenURL = os.Getenv("TOKEN_URL")
+
+	fmt.Println(service.tokenURL)
 	return &service
 }
 
-func GuestHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) GuestHandler(w http.ResponseWriter, r *http.Request) {
+	// redirect to auth-server
+	//http.Redirect(w, r)
 	w.Write([]byte("123123"))
 
 }
